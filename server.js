00d481cca07fcbc7b188dc6755ce3320d9fac4f7
thedon122 <donovan122@yahoo.com>
2018-04-09 14:17:34 -0400

    fix route to index.hbs and added header and footer

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const app = express()
 app.set('view engine', 'hbs')
 app.listen(3000, ()=>{
     console.log("listening on port 3000")
-}) 
-
-app.get("/", (req, res)=> {
-    res.redirect("/users")
 })
-
 // connect to database
 mongoose.connect("mongodb://localhost/regifter")
 
@@ -29,12 +24,17 @@ mongoose.connection.on('error', (error) => {
     process.exit(-1)
 })
 
+app.get("/", (req, res)=> {
+    res.redirect("/users")
+})
+
 const User = require("./models/User")
 app.get("/users", (req, res) =>{
     User.find({})
-    .then((users))
+    .then((users) =>{
     console.log("USERS", users)
     res.render("Users/index", {
         users
     })
+})
 })
