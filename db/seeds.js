5893f351e889c1e4bc12878d20a1216e694c7460
thedon122 <donovan122@yahoo.com>
2018-04-09 14:55:30 -0400

    Updated DB connection code to use MongoLab when in Production

--- db/seeds.js
+++ db/seeds.js
@@ -3,20 +3,21 @@ const Store = require('../models/Store')
 const Gift = require('../models/Gift')
 const mongoose = require('mongoose')
 
-// connect to database
-mongoose.connect("mongodb://localhost/regifter")
-
-mongoose.connection.once('open', () => {
-    console.log(`Mongoose has connected to MongoDB`)
-})
-
-mongoose.connection.on('error', (error) => {
-    console.error(`
-    MongoDB connection error!!! 
-    ${error}
-  `)
-    process.exit(-1)
-})
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/express-movies');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
 
 // Delete all users, then add some fake ones
 User.remove({}, () => {})
--- server.js
+++ server.js
@@ -9,20 +9,21 @@ app.set('view engine', 'hbs')
 app.listen(3000, ()=>{
     console.log("listening on port 3000")
 })
-// connect to database
-mongoose.connect("mongodb://localhost/regifter")
-
-mongoose.connection.once('open', () => {
-    console.log(`Mongoose has connected to MongoDB`)
-})
-
-mongoose.connection.on('error', (error) => {
-    console.error(`
-    MongoDB connection error!!! 
-    ${error}
-  `)
-    process.exit(-1)
-})
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/express-movies');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
 
 app.get("/", (req, res)=> {
     res.redirect("/users")
