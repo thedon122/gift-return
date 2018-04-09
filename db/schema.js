0f198c070fbff14e287bd446e3b7244a0ae38baf
thedon122 <donovan122@yahoo.com>
2018-04-09 11:12:07 -0400

    the server is up and running

--- db/schema.js
+++ db/schema.js
@@ -1,2 +1,23 @@
 const mongoose = require('mongoose')
-const 
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    username: String,
+    text: String,
+    first_name: String,
+    last_name: String,
+    photo_url: String,
+    stores: [Store]
+}) 
+const Store = new Schema({
+    name: String,
+    address: String,
+    gifts: [Gift]
+})
+
+const Gift = new Schema({
+    name: String,
+    description: String,
+    price: Number,
+    gift_giver: String
+})
--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-var express = require('express')
-var hbs = require('hbs')
-var methodOverride = require('method-override')
-var bodyParser = require('body')
+const express = require('express')
+const hbs = require('hbs')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log("listening on port 3000")
+}) 
+
+app.get("/", (req, res)=> {
+    res.send("Hello World")
+})
